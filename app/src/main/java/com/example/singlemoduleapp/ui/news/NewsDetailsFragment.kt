06a8d7038a62ffd6ef42f7440c67d549b463f589
prosenjith <royprosenjith@gmail.com>
2023-09-02 00:55:47 +0600

    Add navigation component

---
+++ app/src/main/java/com/example/singlemoduleapp/ui/news/NewsDetailsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.singlemoduleapp.ui.news
+
+import com.example.singlemoduleapp.R
+import com.example.singlemoduleapp.ui.base.BaseFragment
+
+class NewsDetailsFragment : BaseFragment() {
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_news_details
+}
